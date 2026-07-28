e657ba9aee1488c889e151e67850d29d632e6415
Pavle Milenkovic <pmilenkovic@tenstorrent.com>
2026-07-28 15:40:31 +0000

    Add blaze-authored Blackhole LLKs to experimental

---
+++ tt_metal/hw/inc/api/compute/experimental/generic_moe_gate.h
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "api/compute/compute_kernel_api.h"
+#include "api/compute/common.h"
+#ifdef TRISC_MATH
+#include "experimental/llk_math_deepseek_moe_gate_eltwise_binary_api.h"  // fpu kernel which puts values into tile 0 of dest and values+bias into tile 2
+#include "experimental/llk_sfpu/llk_math_generic_moe_gate_topk_api.h"
+#endif
+
+namespace ckernel {
+
+ALWI void generic_moe_gate_init(uint32_t icb0, uint32_t icb1) {
+    UNPACK((llk_unpack_AB_init<BroadcastType::NONE>(icb0, icb1)));
+
+    // Init copy add (FPU)
+    MATH((llk_math_deepseek_moe_gate_eltwise_binary_init_with_operands<
+          EltwiseBinaryType::ELWADD,
+          DeepseekMoeGateEltwiseBinaryMode::COPY,
+          MATH_FIDELITY>(icb0, icb1, false)));
+    // Init topk (SFPU)
+    MATH((sfpu::llk_math_sfpu_generic_moe_gate_topk_init()));
+}
+
+template <
+    bool normalize = false,
+    int num_selected_experts = 8,
+    int num_total_experts = 256,
+    bool zero_tail = false,
+    bool full_sort = false>
+ALWI void generic_moe_gate(uint32_t icb0, uint32_t icb1, uint32_t eps, uint32_t scale) {
+    // Copy add (FPU)
+    UNPACK((llk_unpack_AB(icb0, icb1, 0, 0)));
+    MATH((llk_math_deepseek_moe_gate_eltwise_binary<EltwiseBinaryType::ELWADD, DST_ACCUM_MODE, MATH_FIDELITY>(
+        icb0, icb1, 0, true)));
+
+    // Topk SFPU
+    MATH((sfpu::llk_math_sfpu_generic_moe_gate_topk<
+          normalize,
+          num_selected_experts,
+          num_total_experts,
+          zero_tail,
+          full_sort>(eps, scale)));
+}
+
+}  // namespace ckernel
---
+++ tt_metal/hw/inc/api/compute/experimental/softmax_k.h
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "api/compute/compute_kernel_api.h"
+#include "api/compute/common.h"
+#include "api/debug/dprint.h"
+#ifdef TRISC_MATH
+#include "experimental/llk_math_unary_datacopy_softmax_k_api.h"
+#include "experimental/llk_sfpu/llk_math_softmax_k_api.h"
+#endif
+#ifdef TRISC_UNPACK
+#include "llk_unpack_A.h"
+#endif
+
+namespace ckernel {
+
+ALWI void softmax_k_init(uint32_t icb) {
+    UNPACK((llk_unpack_A_init<BroadcastType::SCALAR, false, EltwiseBinaryReuseDestType::NONE, false>(false, 1, icb)));
+    MATH((llk_math_eltwise_unary_datacopy_softmax_k_init()));
+    MATH((sfpu::llk_math_sfpu_softmax_k_init()));
+}
+
+template <int k = 8>
+ALWI void softmax_k(uint32_t icb) {
+    // Unpack row0 to srcB
+    UNPACK((llk_unpack_A<BroadcastType::SCALAR, false, EltwiseBinaryReuseDestType::NONE, false>(icb, 0)));
+
+    // B2D direct copy and scalar broadcast.
+    MATH((llk_math_eltwise_unary_datacopy_softmax_k(0)));
+
+    // Softmax
+    MATH((sfpu::llk_math_sfpu_softmax_k<k>()));
+}
+
+}  // namespace ckernel
